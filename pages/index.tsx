bfc357584bcdd73cb8a4b6141227bd60134dfda6
Seren_Modz 21 <seren@kings-world.net>
2022-02-17 18:11:07 +0000

    :sparkles: create a working landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,51 @@
+// import NextImage from "next/image";
+import NextLink from "next/link";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import type { IconType } from "react-icons";
+
 export default function Home() {
   return (
     <div>
-      <p>hello world</p>
+      {/* navbar */}
+      <div className="h-[5vh] w-full bg-gray-200">
+        {/* TODO: add navbar  */}
+      </div>
+
+      {/* content */}
+      <div className="flex h-[90vh] w-full flex-wrap justify-center bg-gray-200">
+        <div className="my-auto items-center justify-center space-y-5 p-5 lg:inline-flex lg:space-x-10 lg:space-y-0">
+          <img
+            src="/logo.png"
+            className="h-[350px] animate-float rounded-full"
+            alt="Seren_Modz 21 Logo"
+          />
+
+          <div>
+            <h1 className="text-5xl font-semibold">Seren_Modz 21</h1>
+            <p className="mt-1 text-3xl">
+              Welcome to my website! My name is Seren_Modz 21.
+            </p>
+
+            <div className="mt-10 flex flex-row space-x-5">
+              <Social href={""} icon={FaGithub} />
+              <Social href={""} icon={FaTwitter} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* footer */}
+      <div className="h-[5vh] w-full bg-gray-200">{/* TODO: add footer */}</div>
     </div>
   );
 }
+
+const Social = (props: { href: string; icon: IconType }) => (
+  <NextLink href={props.href} passHref>
+    <a target="_blank" rel="noopener noreferrer">
+      <button className="inline-flex items-center rounded transition ease-in-out hover:-translate-y-1 hover:scale-110">
+        <props.icon className="h-10 w-10 fill-current" />
+      </button>
+    </a>
+  </NextLink>
+);
