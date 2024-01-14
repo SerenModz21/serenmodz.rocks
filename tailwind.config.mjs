0f636a52bdff5ef2ac21cfdf109ba605e4baaca5
Seren_Modz 21 <seren@kings-world.net>
2024-01-14 23:09:49 +0000

    refactor: replace single quotes with double quotes
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
