97358e772e85a3ce883529f899f3b1f594bdc330
Seren_Modz 21 <seren@kings-world.net>
2025-01-13 22:41:57 +0000

    refactor(astro): update domain for astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.serenmodz.rocks",
+    site: "https://seren.dev",
     integrations: [
         react(),
         tailwind({ applyBaseStyles: false }),
